750597ba5337cd0d996977c50b9b5e3d783bfc30
vikash kumar <vikashkr679223@gmail.com>
2025-02-04 16:46:58 +0530

    RemoveElement

---
+++ LeetCode2025/RemoveElement.java
@@ -0,0 +1,22 @@
+//3,2,2,3,5
+public class RemoveElement{
+    public int removeElement(int []nums,int val){
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[count]=nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String []args){
+        RemoveElement remove=new RemoveElement();
+        int nums[]={2,3,3,2};
+        int val=3;
+        int result=remove.removeElement(nums, val);
+        for(int i=0;i<result;i++){
+            System.out.print(nums[i]);
+        }
+    }
+}
