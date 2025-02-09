e2d3c5b47a6921c3e30186aed592d2dc5d9ea9e1
vikash kumar <vikashkr679223@gmail.com>
2025-02-09 13:19:41 +0530

    LengthOfLastWord

---
+++ LeetCode2025/LengthOfLastWord.java
@@ -0,0 +1,22 @@
+public class LengthOfLastWord {
+public int lengthOfLastWord (String s) {
+	int count=0;
+	for(int i=s.length()-1;i>=0;i--) {
+		if(s.charAt(i)!=' ') {
+			count++;
+			
+		}
+		else if(count >0){
+			return count;
+		}
+	}
+	return -1;
+}
+	public static void main(String[] args) {
+		LengthOfLastWord word=new LengthOfLastWord();
+		String s="  ";
+		int result=word.lengthOfLastWord(s);
+		System.out.print(result);
+	}
+
+}
